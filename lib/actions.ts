29c6e57e000526db6d310310b6c9e5b2255ae66c
LucasLevingston <LucasLevingston>
2023-07-31 13:53:47 -0300

    ...

--- lib/actions.ts
+++ lib/actions.ts
@@ -6,7 +6,7 @@ import { ProjectForm } from "@/common.types";
 const isProduction = process.env.NODE_ENV === 'production';
 const apiUrl = isProduction ? process.env.NEXT_PUBLIC_GRAFBASE_API_URL || '' : 'http://127.0.0.1:4000/graphql';
 const apiKey = isProduction ? process.env.NEXT_PUBLIC_GRAFBASE_API_KEY || '' : 'letmein';
-const serverUrl = isProduction ? process.env.NEXT_PUBLIC_SERVER_URL : 'http://localhost:3000';
+const serverUrl = isProduction ? process.env.NEXT_PUBLIC_SERVER_URL || '' : 'http://localhost:3000';
 
 const client = new GraphQLClient(apiUrl);
 
