75bcb1b18e8c7ea9309e8e859ed22e663c08f55c
LucasLevingston <LucasLevingston>
2023-07-31 14:05:37 -0300

    ...

--- app/page.tsx
+++ app/page.tsx
@@ -30,22 +30,19 @@ export const dynamicParams = true;
 export const revalidate = 0;
 
 const Home = async ({ searchParams: { category, endcursor } }: Props) => {
-  
-    
-    const data = await fetchAllProjects(category, endcursor) as ProjectSearch
-    
-    const projectsToDisplay = data?.projectSearch?.edges || [];
-    
-    if (projectsToDisplay.length === 0) {
-      return (
-        <section className="flexStart flex-col paddings">
+  const data = await fetchAllProjects(category, endcursor) as ProjectSearch
+
+  const projectsToDisplay = data?.projectSearch?.edges || [];
+
+  if (projectsToDisplay.length === 0) {
+    return (
+      <section className="flexStart flex-col paddings">
         <Categories />
-        
+
         <p className="no-result-text text-center">No projects found, go create some first.</p>
-        </section>
-        )
-      }
-  
+      </section>
+    )
+  }
 
   return (
     <section className="flexStart flex-col paddings mb-16">
@@ -73,7 +70,6 @@ const Home = async ({ searchParams: { category, endcursor } }: Props) => {
       />
     </section>
   )
-    
 };
 
 export default Home;
