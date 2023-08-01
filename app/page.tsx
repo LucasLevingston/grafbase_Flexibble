8a27240db29d4ebfe6a19ddf2966e378f80e378d
LucasLevingston <LucasLevingston>
2023-08-01 15:11:59 -0300

    teste

--- app/page.tsx
+++ app/page.tsx
@@ -30,6 +30,7 @@ export const dynamicParams = true;
 export const revalidate = 0;
 
 const Home = async ({ searchParams: { category, endcursor } }: Props) => {
+	//@ts-ignore
 	const data = (await fetchAllProjects(category, endcursor)) as ProjectSearch;
 
 	const projectsToDisplay = data?.projectSearch?.edges || [];
